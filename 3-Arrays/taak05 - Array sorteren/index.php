51b2e5ea55e5d82bfecac2e2a4c7fcc014d47bd8
mitchel van cronenburg <mitchel.vancronenburg@student.rocva.nl>
2020-11-26 12:32:22 +0100

    gedaan

---
+++ 3-Arrays/taak05 - Array sorteren/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$getallen = ['676', '5345', '78657','3', '46', '21', '47'];
+$laag_hoog = ;
+$hoog_laag = arsort($getallen);
+print_r($laag_hoog);
+
+?>
+
+</body>
+</html>
---
+++ 4-Arithmetics/taak01 - Arithmetic Operators/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$getal1 = 6;
+$getal2 = 10;
+echo $getal2 - $getal1;
+echo "<br>";
+echo $getal1 + $getal2;
+echo "<br>";
+echo $getal1 * $getal2;
+echo "<br>";
+echo $getal1 / $getal2;
+?>
+
+</body>
+</html>
