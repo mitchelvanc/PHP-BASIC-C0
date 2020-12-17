e30248ff0435243a3c6dccb9b9f118f3427c2e62
mitchel van cronenburg <mitchel.vancronenburg@student.rocva.nl>
2020-12-17 01:22:25 +0100

    gedaan

---
+++ 4-Arithmetics/taak02 -Assignment Operators/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$getal1 = 1;
+
+
+echo $getal1-10;
+echo '<br>';
+echo $getal1+10;
+echo '<br>';
+echo $getal1*10;
+echo '<br>';
+echo $getal1/10;
+?>
+
+</body>
+</html>
