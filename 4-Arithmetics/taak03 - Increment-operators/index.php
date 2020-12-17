24478eec1077ed420595c94ed0e36fbbd6dc5f37
mitchel van cronenburg <mitchel.vancronenburg@student.rocva.nl>
2020-12-17 01:49:10 +0100

    gedaan

---
+++ 4-Arithmetics/taak03 - Increment-operators/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+</html>
+<body>
+    
+    <?php
+        $getal1 = 0;
+        echo ++$getal1;
+        echo '<br>';
+        echo --$getal1;
+    ?>
+
+</body>
+</html>
---
+++ 5-Conditional_Statements/taak01 - Comparison Operators/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+</html>
+<body>
+    
+    <?php
+        $test1 = 12;
+        $test2 = "gewoon text";
+
+        var_dump($test1 == $test2);
+        var_dump($test1 === $test2);
+        var_dump($test1 != $test2);
+        var_dump($test1 > $test2);
+        var_dump($test1 < $test2);
+        var_dump($test1 <> $test2);
+        var_dump($test1 >= $test2);
+        var_dump($test1 <= $test2);
+
+    ?>
+
+</body>
+</html>
